538e2e9aa65410ecee57b14d834e1c7615467405
Ryan <rminsky42@gmail.com>
2026-07-15 01:28:40 -0700

    KDE - Fix plasma-manager warning

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+          home-manager.sharedModules = [ plasma-manager.homeModules.plasma-manager ];
 
           home-manager.users.rm = import ./environments/KDE/home.nix;
         }
